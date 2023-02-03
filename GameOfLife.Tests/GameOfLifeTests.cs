85a71b62abc80cec921affa71b6347d48456d947
sokolovaeldm <sokolova.el.dm@gmail.com>
2023-02-03 15:40:01 +0100

    add States Alive and Dead

--- GameOfLife.Tests/GameOfLifeTests.cs
+++ GameOfLife.Tests/GameOfLifeTests.cs
@@ -2,16 +2,19 @@ namespace GameOfLife.Tests;
 
 public class GameOfLifeTests
 {
+
     [Theory]
     [InlineData(true, 1, false)]
     [InlineData(true, 2, true)]
     [InlineData(true, 3, true)]
     [InlineData(true, 4, false)]
     [InlineData(false, 3, true)]
-    public void GetCellState(bool isAlive, int aliveNeighbours, bool expectedNextGen)
+    public void GetCellState(bool isAlive, int aliveNeighbours, bool survived)
     {
         var cell = new Cell(isAlive);
-        Assert.Equal(expectedNextGen, cell.NextGen(aliveNeighbours));
+        cell.SurvivedWith(aliveNeighbours);
+        
+        Assert.Equal(survived, cell.IsAlive());
 
     }
 }
--- GameOfLife/Cell.cs
+++ GameOfLife/Cell.cs
@@ -2,19 +2,62 @@ namespace GameOfLife;
 
 public class Cell
 {
-    private readonly bool _isAlive;
+    private IState _state;
 
     public Cell(bool isAlive)
     {
-        _isAlive = isAlive;
+        _state = isAlive ? new AliveState() : new DeadState();
     }
 
-    public bool NextGen(int aliveNeighbours)
+    public bool IsAlive()
     {
+        return _state.IsAlive();
+    }
+
+    public void SurvivedWith(int aliveNeighbours)
+    {
+        _state = _state.SurvivedWith(aliveNeighbours);
+    }
+}
+
+public interface IState
+{
+    IState SurvivedWith(int aliveNeighbours);
+    bool IsAlive();
+}
+
+public class AliveState : IState
+{
+    public IState SurvivedWith(int aliveNeighbours)
+    {
+        
         if(aliveNeighbours == 2 || aliveNeighbours == 3)
         {
-            return true;
+            return this;
         }
+        return new DeadState();
+    }
+
+    public bool IsAlive()
+    {
+        return true;
+    }
+}
+
+public class DeadState : IState
+{
+    public IState SurvivedWith(int aliveNeighbours)
+    {
+        if (aliveNeighbours == 3)
+        {
+            return new AliveState();
+        }
+
+        return this;
+    }
+
+    public bool IsAlive()
+    {
         return false;
     }
 }
